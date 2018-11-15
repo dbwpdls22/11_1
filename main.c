4e6fcf9dce1b041d9cba989377dd334557b28b7f
dbwpdls22 <dbwpdls22@naver.com>
2018-11-15 15:42:14 +0900

    11_4

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-char *proverb="All thatt glisters is not gold.";
-void setPointer(char *q)
+char *proverb="All that glister is not gold.";
+void setPointer(char **q)
 {
-	q=proverb;
+	*q=proverb;
 }
 int main(int argc, char *argv[]) {
 	char *p="zzz";
-	setPointer(p);
+	setPointer(&p);
 	printf("%s\n",p);
 	
 	return 0;
